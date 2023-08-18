13c18553d03e92929e7fa32e020b5db896ad18cf
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-18 19:57:49 +0800

    million

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
+import million from 'million/compiler'
 import react from '@vitejs/plugin-react-swc'
 import generouted from '@generouted/react-router/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    million.vite({ auto: true }),
     react(),
     generouted(),
   ],
