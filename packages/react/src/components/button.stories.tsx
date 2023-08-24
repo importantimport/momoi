1ea7c63b70c6efc561a9cfdd1a5f21e38532c7bf
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-24 19:55:07 +0800

    tsconfig paths

--- packages/react/src/components/button.stories.tsx
+++ packages/react/src/components/button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Story } from '@ladle/react'
 
-import { Button } from './button'
+import { Button } from '~/components/button'
 
 export const ButtonDemo: Story = () => (
   <Button>Button</Button>
