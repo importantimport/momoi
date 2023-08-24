d1cd0165727540708319fbd93311c414665a20b2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-24 19:55:54 +0800

    revert

--- packages/react/src/components/button.stories.tsx
+++ packages/react/src/components/button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Story } from '@ladle/react'
 
-import { Button } from '~/components/button'
+import { Button } from './button'
 
 export const ButtonDemo: Story = () => (
   <Button>Button</Button>
