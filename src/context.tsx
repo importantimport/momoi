cf508c1c18401917d14ccc1299464829c233fcf7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-08 22:11:49 +0800

    context

---
+++ src/context.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { ReactRouterConcurrentNavigationProvider } from 'foxact/use-react-router-enable-concurrent-navigation'
+
+import { MatrixClientProvider } from '~/context/matrix-client'
+
+export { MatrixClientProvider, ReactRouterConcurrentNavigationProvider }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,28 @@
+import { ComposeContextProvider } from 'foxact/compose-context-provider'
 import { Outlet } from 'react-router-dom'
 import { createHead } from 'unhead'
 
 import { Header } from '~/components/header'
+import { MatrixClientProvider, ReactRouterConcurrentNavigationProvider } from '~/context'
 
 import './app.css'
 
 createHead()
 
+/* eslint-disable react/jsx-key */
+const contexts = [
+  <MatrixClientProvider />,
+  <ReactRouterConcurrentNavigationProvider />,
+]
+/* eslint-enable react/jsx-key */
+
 const App = () => (
-  <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-    </>
+  <ComposeContextProvider contexts={contexts}>
+    <Header />
+    <main>
+      <Outlet />
+    </main>
+  </ComposeContextProvider>
 )
 
 App.displayName = 'Momoi App'
