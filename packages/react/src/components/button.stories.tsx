bd71cb1c061a22906c46e807f7ae07187ca9873b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-08 12:42:18 +0800

    Update button.stories.tsx

--- packages/react/src/components/button.stories.tsx
+++ packages/react/src/components/button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Story } from '@ladle/react'
 
-import { Button as MButton } from './button'
+import { Button as MButton } from '~/components/button'
 
 export const Button: Story = () => (
   <MButton>Button</MButton>
