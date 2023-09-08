7683e8e48ef36f66310d9fcf55b0525c0898765b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-08 23:04:55 +0800

    fix glob

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     // glob all tsx file (without stories)
     .sync('src/**/*.tsx', { ignore: ['src/**/*.stories.tsx'] })
     // 'src/components/button.tsx' => ['components/button', 'src/components/button.tsx']
-    .map(entry => [entry.slice(4), entry]),
+    .map(entry => [entry.slice(4, -4), entry]),
   ),
   format: ['esm'],
 })
