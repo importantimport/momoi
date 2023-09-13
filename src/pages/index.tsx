c92da15cf517b0212a22a2e3569193159609d712
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-13 20:50:02 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,55 @@
-import viteLogo from '/vite.svg'
-import { useState } from 'react'
+// import viteLogo from '/vite.svg'
+import { useLocalStorage } from '@mantine/hooks'
 
-import reactLogo from '~/assets/react.svg'
+// import { useState } from 'react'
+
+// import reactLogo from '~/assets/react.svg'
 import { useMatrixClient } from '~/context'
+import { Navigate } from '~/router'
 
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  const client = useMatrixClient()
+  const matrixClient = useMatrixClient()
+  const [accessToken] = useLocalStorage({ key: 'momoi-access-token' })
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img alt="Vite logo" className="logo" src={viteLogo} />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img alt="React logo" className="logo react" src={reactLogo} />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      {client ? 'client' : 'undefined'}
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (matrixClient?.isLoggedIn) {
+    // return <Navigate to="/room" />
+  }
+  else if (accessToken) {
+    matrixClient?.loginWithToken(accessToken)
+    // return <Navigate to="/room" />
+  }
+  else {
+    return <Navigate to="/login" />
+  }
+
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
+  //         <img alt="Vite logo" className="logo" src={viteLogo} />
+  //       </a>
+  //       <a href="https://react.dev" rel="noreferrer" target="_blank">
+  //         <img alt="React logo" className="logo react" src={reactLogo} />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount(count => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
 }
 
 export default App
