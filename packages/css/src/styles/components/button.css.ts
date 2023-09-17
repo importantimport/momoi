22e12c67c14642c7e0b28a79530c5aa9426eba4f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-17 20:51:54 +0800

    base button style

--- packages/css/src/styles/components/button.css.ts
+++ packages/css/src/styles/components/button.css.ts
@@ -1,21 +1,25 @@
 import '@vanilla-extract/css'
 import { type RecipeVariants, recipe } from '@vanilla-extract/recipes'
 
+import { vars } from '~/vars.css'
+
 export const button = recipe({
   base: {
-    ':hover': {
-      backgroundColor: 'gray',
+    backgroundColor: vars.color.neutral[1], // black
+    borderRadius: 4,
+    boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+    color: `color-mix(in srgb, ${vars.color.neutral[11]} 50%, ${vars.color.neutral[12]})`, // white
+  },
+  defaultVariants: { size: 'small' },
+  variants: {
+    size: {
+      medium: {
+        padding: 12,
+      },
+      small: {
+        padding: 8,
+      },
     },
-    'alignItems': 'center',
-    'backgroundColor': 'white',
-    'borderRadius': '4px',
-    'color': 'black',
-    'display': 'inline-flex',
-    'fontWeight': 500,
-    'height': 36,
-    'justifyContent': 'center',
-    'lineHeight': 1,
-    'padding': '0 15px',
   },
 })
 
