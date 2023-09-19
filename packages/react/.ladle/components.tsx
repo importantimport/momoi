09600d79ad934b6be584dc649bc97d3f92e8099a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-19 15:12:57 +0800

    ignore prop-types

--- packages/react/.ladle/components.tsx
+++ packages/react/.ladle/components.tsx
@@ -4,7 +4,9 @@ import * as React from 'react'
 import '@momoi/css/reset.css'
 import '@momoi/css/themes/pink.css'
 
+// eslint-disable-next-line react/prop-types
 export const Provider: GlobalProvider = ({ children, globalState: { theme } }) => (
+  // eslint-disable-next-line react/prop-types
   <div className={theme.toLowerCase()}>
     {children}
   </div>
