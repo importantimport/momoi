1195ccf1ed60be6177cf30efe2cdf76aa17acb0f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-19 14:30:57 +0800

    test eslint config

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import react from '@importantimprot/eslint-config-react'
+import vanillaExtract from '@importantimport/eslint-config-vanilla-extract'
+
+export default [
+  ...react,
+  ...vanillaExtract,
+]
