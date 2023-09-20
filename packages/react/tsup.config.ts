a8dd35b582fcbf852f7f35645ee2b7b2e76808d0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-20 12:54:50 +0800

    set external

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -9,5 +9,6 @@ export default defineConfig({
     // 'src/components/button.tsx' => ['components/button', 'src/components/button.tsx']
     .map(entry => [entry.slice(4, -4), entry]),
   ),
+  external: ['react'],
   format: ['esm'],
 })
