7d7237490fd59154246f6b8575c59d5489e5843d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-21 23:15:27 +0800

    format code

--- src/utils/hooks/use-secure-local-storage.ts
+++ src/utils/hooks/use-secure-local-storage.ts
@@ -1,21 +1,22 @@
-import { useLocalStorage } from "@mantine/hooks"
+import { useLocalStorage } from '@mantine/hooks'
 
-import EncryptionService from "react-secure-storage/src/lib/encryption.ts"
+import EncryptionService from 'react-secure-storage/src/lib/encryption.ts'
 
-export const useSecureLocalStorage = <T = string>(props: Parameters<typeof useLocalStorage<T>>[0]) => {
-  const encryption = new EncryptionService()
+export const useSecureLocalStorage =
+  <T = string>(props: Parameters<typeof useLocalStorage<T>>[0]) => {
+    const encryption = new EncryptionService()
 
-  return useLocalStorage<T>({
-    ...props,
-    serialize: value => encryption.encrypt(JSON.stringify(value)),
-    deserialize: value => {
-      if (value) {
-        try {
-          return JSON.parse(encryption.decrypt(value))
-        } catch {
-          return encryption.decrypt(value)
-        }
-      } else return value
-    },
-  })
-}
+    return useLocalStorage<T>({
+      ...props,
+      serialize: value => encryption.encrypt(JSON.stringify(value)),
+      deserialize: value => {
+        if (value) {
+          try {
+            return JSON.parse(encryption.decrypt(value))
+          } catch {
+            return encryption.decrypt(value)
+          }
+        } else return value
+      },
+    })
+  }
