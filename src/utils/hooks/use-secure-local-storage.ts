6cca3e9d8c25aa7d1d13142fe0d6599ae60790e5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-21 23:12:53 +0800

    secure-local-storage fix

--- src/utils/hooks/use-secure-local-storage.ts
+++ src/utils/hooks/use-secure-local-storage.ts
@@ -3,17 +3,17 @@ import { useLocalStorage } from "@mantine/hooks"
 import EncryptionService from "react-secure-storage/src/lib/encryption.ts"
 
 export const useSecureLocalStorage = <T = string>(props: Parameters<typeof useLocalStorage<T>>[0]) => {
-  const { decrypt, encrypt } = new EncryptionService()
+  const encryption = new EncryptionService()
 
   return useLocalStorage<T>({
     ...props,
-    serialize: value => encrypt(JSON.stringify(value)),
+    serialize: value => encryption.encrypt(JSON.stringify(value)),
     deserialize: value => {
       if (value) {
         try {
-          return JSON.parse(decrypt(value))
+          return JSON.parse(encryption.decrypt(value))
         } catch {
-          return decrypt(value)
+          return encryption.decrypt(value)
         }
       } else return value
     },
